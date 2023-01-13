bbd8b36f955fb318ba752b2bab270f3252ecee16
Rhmani <60844671+Rhmani@users.noreply.github.com>
2023-01-13 21:12:47 +0300

    filtering is ready

--- src/app/admin/donationsreport/donationsreport.component.ts
+++ src/app/admin/donationsreport/donationsreport.component.ts
@@ -48,14 +48,14 @@ export class DonationsreportComponent implements OnInit  {
 
 
     debugger;
-    if(this.searchForm.controls['date1'].value==null){
+    if(this.searchForm.controls['date1'].value==null || date1==null){
       this.home.alldonation= this.home.alldonation.filter((x:any)=> date2>= x.datedonation)
 
     }
-    else if(this.searchForm.controls['date2'].value==null){
+    else if(this.searchForm.controls['date2'].value==null || date2==null){
       this.home.alldonation= this.home.alldonation.filter((x:any)=> date1 <= x.datedonation)
     }
-   else{
+    else{
     this.home.alldonation= this.home.alldonation.filter((x:any)=> date1 <= x.datedonation && date2>= x.datedonation)}
 
    }
